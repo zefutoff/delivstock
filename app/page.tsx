439d8928e29a716df0d3e4b77cc274572b98dc22
zefut <zefut.pro@gmail.com>
2024-03-03 20:24:22 +0100

    Suppression des imports inutilisés

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 import { LoginButton } from "@/components/auth/login-button";
-import CardCustom from "@/components/card";
-import { Button } from "@nextui-org/button";
 
 export default function Home() {
   return (
-   <main className="flex h-full flex-col items-center justify-center">
+    <main className="flex h-full flex-col items-center justify-center">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-semibold text-withe drop-shadow-md">
           Auth
         </h1>
-        <p className="text-withe text-lg">
-          Connexion
-        </p>
+        <p className="text-withe text-lg">Connexion</p>
         <div>
           <LoginButton>
             <button>se connecter</button>
           </LoginButton>
-          
         </div>
       </div>
-   </main>
+    </main>
   );
 }
