fb983a628a2d075a10225b575a5bc9e391fb63b3
zefut <zefut.pro@gmail.com>
2024-03-10 14:52:19 +0100

    Ajout des paramétres de compte

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,33 +1,52 @@
 "use client";
 
-import { logout } from "@/action/logout";
 import { TitlePage } from "@/components/title-page";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import Navbar from "@/components/ui/navbar";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { Separator } from "@/components/ui/separator";
+import { ChevronRight } from "react-feather";
+import { LogoutButton } from "@/components/auth/logout-button";
 
 const SettingsPage = () => {
   const user = useCurrentUser();
-
-  const onClick = () => {
-    logout();
-  };
 
   return (
     <>
       <Navbar />
       <TitlePage label="Paramètre" />
-      <Card className="mt-2 w-11/12">
-        <CardContent className="items-center">
-          <p>Nom {user?.name}</p>
-          <p>adresse mail {user?.email}</p>
-          <p>Thème</p>
-          <p>Langue</p>
-        </CardContent>
+      <Card className="w-11/12">
+        <div className="m-1.5 flex cursor-pointer justify-between">
+          <p className="mb-0">Nom</p>
+          <div className="flex items-center">
+            <p className="font-semibold mb-0">{user?.name}</p>
+            <ChevronRight color="Grey" />
+          </div>
+        </div>
+
+        <Separator className="bg-Grey" />
+        <div className="m-1.5 flex cursor-pointer justify-between">
+          <p>adresse mail</p>
+          <div className="flex items-center">
+            <p className="font-semibold">{user?.email}</p>
+            <ChevronRight color="Grey" />
+          </div>
+        </div>
+        <Separator className="bg-Grey" />
+        <div className="m-1.5">
+          <label className="flex cursor-pointer items-center justify-between">
+            <p>Thème</p>
+          </label>
+        </div>
+        <Separator className="bg-Grey" />
+        <div className="m-1.5">
+          <label className="flex cursor-pointer items-center justify-between">
+            <p>Langue</p>
+            <ChevronRight color="Grey" />
+          </label>
+        </div>
       </Card>
-      <button onClick={onClick} className="font-semibold text-red-600">
-        Déconnexion
-      </button>
+      <LogoutButton>Déconnexion</LogoutButton>
     </>
   );
 };
