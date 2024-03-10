2926a8952dd9f1929dff25c9295dc057029ad71d
zefut <zefut.pro@gmail.com>
2024-03-10 14:50:15 +0100

    Ajoout d'un marge

--- components/title-page.tsx
+++ components/title-page.tsx
@@ -6,7 +6,7 @@ interface TitlePageProps {
 
 export const TitlePage = ({ label }: TitlePageProps) => {
   return (
-    <div className="flex flex-col w-full items-center md:hidden">
+    <div className="flex flex-col w-full items-center -mb-7 md:hidden">
       <p className="font-bold items-center m-3">{label}</p>
       <Separator className="bg-Grey" />
     </div>
