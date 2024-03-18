87d7c095942905051f688f57a562c22c16463541
zefut <zefut.pro@gmail.com>
2024-03-18 01:01:08 +0100

    Composant inutilisé

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
