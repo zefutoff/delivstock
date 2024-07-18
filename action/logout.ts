3d3d459eaf206262b2c73dd3c058b88c8999ff55
zefut <zefut.pro@gmail.com>
2024-07-18 23:45:00 +0200

    Redurection apres la deconnexion

--- action/logout.ts
+++ action/logout.ts
@@ -1,7 +1,14 @@
 "use server";
 
 import { signOut } from "@/auth";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
 export const logout = async () => {
-  await signOut();
+  await signOut({
+    redirect: true,
+    redirectTo: `/`,
+  });
 };
